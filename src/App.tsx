fde09cd4aa2da2fe06d5be856d93e704d99718ba
Konrad Plonka <konrad66a4@gmail.com>
2024-10-16 08:58:20 +0200

    change app name

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ const App: React.FC = () => {
     <RecommendationProvider>
       <div className="max-w-full h-screen overflow-hidden   bg-gradient-to-r from-[#FD297B] via-[#FF5864] to-[#FF655B]">
         <header className="p-6 absolute text-[#424242]">
-          <h1 className="text-3xl font-bold">scrinder</h1>
+          <h1 className="text-3xl font-bold">screender</h1>
         </header>
         <main className='overflow-hidden'>
           <Swiper />
         </main>
       </div> 
-xxx    </RecommendationProvider >
+   </RecommendationProvider >
   );
 };
 
