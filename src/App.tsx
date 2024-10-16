8d67f5421979be98bae82935ab47af7cd824a304
Konrad Plonka <konrad66a4@gmail.com>
2024-10-16 08:43:02 +0200

    setup gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { RecommendationProvider } from './context/RecomendationContext';
-import 'react-toastify/dist/ReactToastify.css';
 import Swiper from './components/Swiper';
-
+import './index.css'
 
 const App: React.FC = () => {
   return (
